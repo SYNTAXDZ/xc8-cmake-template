e3c6d81cf8ea022a641b4e896c89ab51fb48a5ca
SYNTAXDZ <miloudiadelhani@gmail.com>
2021-08-02 20:25:18 +0100

    getting this template to work with pic16f84a

--- hal/src/hal.c
+++ hal/src/hal.c
@@ -1,7 +1,13 @@
 #include "hal/hal.h"
+
+#include <pic16f84a.h>
+#include <pic.h>
 #include <xc.h>
 
 void hal_init(void)
 {
-    LATCbits.LATC1 = 0;
+    //LATCbits.LATC1 = 0;
+    TRISB = 0x00;
+    PORTB = 0x01;
+
 }
